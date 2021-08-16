a8267cfd6bdad7b9a467554f0a56dcb319541172
Your Name <pronaybaura9@gmail.com>
2021-08-16 12:02:59 +0600

    update

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify:false
+    useFindAndModify:true
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
